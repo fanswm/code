fd40afddf400bad2fb4af2fae750110542dc3ea9
fan_yue_hui <fan_yue_hui@163.com>
2018-04-05 17:35:01 +0800

    update by gitbash at jhk

--- java/Test.java
+++ java/Test.java
@@ -1,10 +1,10 @@
 public class Test {
     public static void main(String args[]) {
-        for(int i=1; i<=9; i++){
-            for(int j=1; j<=i; j++ ){
-                System.out.print(j + "*" + i + "=" + i*j + " ");
-            }
-            System.out.println();
-        }
+        String Str = new String("    www.runoob.com    ");
+        System.out.print("base:" );
+        System.out.println( Str );
+
+        System.out.print("After:" );
+        System.out.println( Str.trim() );
     }
 }
