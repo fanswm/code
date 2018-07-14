f5d568ab99c773463c2789657f9fdbcd25160f42
fan_yue_hui <fan_yue_hui@163.com>
2018-07-14 09:19:37 +0800

    update by gitbash at jhk

--- python/tmp.py
+++ python/tmp.py
@@ -1,1 +1,33 @@
-print('\n'.join([''.join([('PYTHON!'[(x-y)%7]if((x*0.05)**2+(y*0.1)**2-1)**3-(x*0.05)**2*(y*0.1)**3<=0 else ' ')for x in range(-30,30)]) for y in range(15,15,-1)]))
+# -*- coding: utf-8 -*-
+
+def triangles():
+	L = [1] 
+	while True: 
+		yield L 
+		L= [
+			(L + [0])[i] + ([0] + L)[i] for i in range(len(L)+1)
+			]
+n = 0
+results = []
+for t in triangles():
+	print(t)
+	results.append(t)
+	n = n + 1
+	if n == 10:
+		break
+
+if results == [
+	[1],
+	[1, 1],
+	[1, 2, 1],
+	[1, 3, 3, 1],
+	[1, 4, 6, 4, 1],
+	[1, 5, 10, 10, 5, 1],
+	[1, 6, 15, 20, 15, 6, 1],
+	[1, 7, 21, 35, 35, 21, 7, 1],
+	[1, 8, 28, 56, 70, 56, 28, 8, 1],
+	[1, 9, 36, 84, 126, 126, 84, 36, 9, 1]
+	]:
+	print('done!')
+else:
+	print('error!')
